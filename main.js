288dfeb0a0448accefa6105505071bc96eb00871
Javier Vargas <javier.vargas@globant.com>
2018-01-14 15:34:29 -0500

    add basic animation

--- main.js
+++ main.js
@@ -5,9 +5,27 @@ function startDrag (event) {
 function startDrop (event) {
     event.preventDefault();
     var data = event.dataTransfer.getData('text');
-    event.target.appendChild(document.getElementById(data));
+    if( event.target.nodeName !== 'A' ) {
+        event.target.appendChild(document.getElementById(data));    
+    }
+    else {
+        alert("ya existe un elemento en esta zona"); 
+    }
 }
 
 function allowDrop(event) {
     event.preventDefault();
 }
+
+var activeModal = document.getElementsByClassName('active_modal');
+
+for (var i = 0; i < activeModal.length; i++) {
+    activeModal[i].addEventListener('click', function() {
+        var person = document.querySelector('.person');
+        var positionPerson = this.getBoundingClientRect();
+        person.setAttribute('style', `top: ${positionPerson.top}px; left: ${positionPerson.left}px;`);
+    }, false);
+}
+
+
+
